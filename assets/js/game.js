6280914354bd0e6f2cfef2c0e61752fc0b562b05
Dearon <dearon@gmail.com>
2013-10-26 23:21:54 +0200

    Removed the 960.gs dependency Added a sidebar Moving now updates the visuals

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,10 +6,20 @@ levels[1]['layout'] = [[' ', ' ', ' '],
 levels[1]['start'] = ['1', '1'];
 
 var blockSize = 50;
+var sidebarWidth = 205;
 
 var colors = {};
 colors[' '] = '#fff';
 colors['r'] = '#ff0000';
+colors['o'] = '#ff7f00';
+colors['y'] = '#ffff00';
+colors['g'] = '#00ff00';
+colors['b'] = '#0000ff';
+colors['i'] = '#4b0082';
+colors['v'] = '#8f00ff';
+colors['background'] = '#181818';
+colors['sidebar_bg'] = '#D63D22';
+colors['game_bg'] = '#303030';
 
 var state = {};
 state['level'] = '1';
@@ -24,18 +34,40 @@ function init() {
 	state['position'] = levels[state['level']]['start'];
 	state['color'] = level['layout'][state['position'][0]][state['position'][1]];
 
-	drawLevel(stage);
+	draw(stage);
 	runGame(stage);
 }
 
-function drawLevel(stage) {
+function draw(stage) {
 	stage.clear();
+	drawSidebar(stage);
+	drawLevel(stage);
+}
 
+function drawSidebar(stage) {
+	var bg = new createjs.Shape();
+	bg.graphics.beginFill(colors['sidebar_bg']).drawRect(0, 0, sidebarWidth, stage.canvas.height);
+	stage.addChild(bg);
+	stage.update();
+
+	var border = new createjs.Shape();
+	border.graphics.beginFill(colors['background']).drawRect(sidebarWidth - 5, 0, 5, stage.canvas.height);
+	stage.addChild(border);
+	stage.update();
+
+}
+
+function drawLevel(stage) {
 	var level = levels[state['level']];
 	var width = level['layout'][0].length * 50;
 	var height = level['layout'].length * 50;
-	var xStart = (stage.canvas.width - width) / 2;
+	var xStart = ((stage.canvas.width + sidebarWidth) - width) / 2;
 	var yStart = (stage.canvas.height - height) / 2;
+
+	var bg = new createjs.Shape();
+	bg.graphics.beginFill(colors['game_bg']).drawRect(sidebarWidth, 0, stage.canvas.width - sidebarWidth, stage.canvas.height);
+	stage.addChild(bg);
+	stage.update();
 
 	var block = new createjs.Shape();
 
@@ -65,7 +97,7 @@ function runGame(stage) {
 				state['position'][1] = newPos;
 				level['layout'][state['position'][1]][state['position'][0]] = state['color'];
 
-				drawLevel(stage);
+				draw(stage);
 
 				console.log('Up: ' + state['position']);
 			}
@@ -79,7 +111,7 @@ function runGame(stage) {
 				state['position'][1] = newPos;
 				level['layout'][state['position'][1]][state['position'][0]] = state['color'];
 
-				drawLevel(stage);
+				draw(stage);
 
 				console.log('Down: ' + state['position']);
 			}
@@ -93,7 +125,7 @@ function runGame(stage) {
 				state['position'][0] = newPos;
 				level['layout'][state['position'][1]][state['position'][0]] = state['color'];
 
-				drawLevel(stage);
+				draw(stage);
 
 				console.log('Left: ' + state['position']);
 			}
@@ -107,7 +139,7 @@ function runGame(stage) {
 				state['position'][0] = newPos;
 				level['layout'][state['position'][1]][state['position'][0]] = state['color'];
 
-				drawLevel(stage);
+				draw(stage);
 
 				console.log('Right: ' + state['position']);
 			}
