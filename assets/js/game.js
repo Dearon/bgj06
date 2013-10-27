540147d13dac020c8a6c5c7b5ea6eaab855eb446
Dearon <dearon@gmail.com>
2013-10-27 16:19:56 +0100

    Added level transitions, levels are now copied, sidebar text is displayed corrently and so forth

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,31 +2,36 @@ function init() {
 	var canvas = document.getElementById("game");
 	var stage = new createjs.Stage(canvas);
 
-	var level = levels[state['level']];
-	state['position'] = levels[state['level']]['start'];
-	state['color'] = level['layout'][state['position'][0]][state['position'][1]];
+	state['level'] = $.extend({}, levels[1]);
+	state['levelNumber'] = 1;
+	state['position'] = state['level']['start'];
+	state['color'] = state['level']['layout'][state['position'][0]][state['position'][1]];
 
-	update(stage, level);
+	check(stage);
+	update(stage);
 
 	Mousetrap.bind('up', function() {
 		var newPos = [state['position'][0], parseInt(state['position'][1]) - 1];
-		handleInput(stage, level, newPos);
+		handleInput(stage, newPos);
 	});
 	Mousetrap.bind('down', function() {
 		var newPos = [state['position'][0], parseInt(state['position'][1]) + 1];
-		handleInput(stage, level, newPos);
+		handleInput(stage, newPos);
 	});
 	Mousetrap.bind('left', function() {
 		var newPos = [parseInt(state['position'][0]) - 1, state['position'][1]];
-		handleInput(stage, level, newPos);
+		handleInput(stage, newPos);
 	});
 	Mousetrap.bind('right', function() {
 		var newPos = [parseInt(state['position'][0]) + 1, state['position'][1]];
-		handleInput(stage, level, newPos);
+		handleInput(stage, newPos);
 	});
 }
 
-function update(stage, level) {
+function update(stage) {
+	stage.clear();
+	var level = state['level'];
+
 	// Create the background
 	var bg = new createjs.Shape();
 	bg.graphics.beginFill(colors['sidebar_bg']).drawRect(0, 0, sidebarWidth, stage.canvas.height);
@@ -46,20 +51,17 @@ function update(stage, level) {
 	text.y = 10;
 	stage.addChild(text);
 
-	for (var i = 0; i < level['goals'].length; i++) {
-		var goal = level['goals'][i];
+	var text = new createjs.Text("", "15px Arial", colors['sidebar_text']);
 
-		var blocksNeeded = goal[1];
-		var colorName = colorNames[goal[0]];
+	$.each(level['goalsLeft'], function(index, item) {
+		if (index != 'total') {
+			text.text += item + " blocks of " + colorNames[index] + " left.\n\n";
+		}
+	});
 
-		var text = new createjs.Text(blocksNeeded + " blocks of " + colorName + " needed.", "15px Arial", colors['sidebar_text']);
-		text.x = 10;
-		text.y = 45 * (i + 1);
-		stage.addChild(text);
-	}
-
-	// Render what we've set up so far
-	stage.update();
+	text.x = 10;
+	text.y = 45;
+	stage.addChild(text);
 
 	// Create the puzzle
 	var width = level['layout'][0].length * 50;
@@ -70,7 +72,7 @@ function update(stage, level) {
 
 	for(var i = 0; i < level['layout'].length; i++) {
 		for(var j = 0; j < level['layout'][i].length; j++) {
-			var tile = level['layout'][i][j];
+			var tile = level['layout'][i][j].toLowerCase();
 			var x = xStart + (50 * j);
 			var y = yStart + (50 * i);
 
@@ -78,23 +80,81 @@ function update(stage, level) {
 			block.graphics.beginFill(colors[tile]).drawRect(x, y, blockSize, blockSize);
 			stage.addChild(block);
 
-			stage.update();
+			if (state['position'][0] == j && state['position'][1] == i) {
+				var circle = new createjs.Shape();
+				circle.graphics.setStrokeStyle(4).beginStroke(colors['circle']);
+				circle.graphics.drawCircle(x + 25, y + 25, 10);
+				stage.addChild(circle);
+			}
+
 		}
 	}
+
+	// Show the whole thing
+	stage.update();
 }
 
-function handleInput(stage, level, newPos) {
+function handleInput(stage, newPos) {
+	var level = state['level'];
 	var width = level['layout'][0].length - 1;
 	var height = level['layout'].length - 1;
 
 	var newPosX = newPos[0];
 	var newPosY = newPos[1];
 
-	if (typeof level['layout'][newPosY] != 'undefined' && typeof level['layout'][newPosY][newPosX] != 'undefined' && level['layout'][newPosY][newPosX] == ' ') {
-		state['position'][0] = newPosX;
-		state['position'][1] = newPosY;
-		level['layout'][newPosY][newPosX] = state['color'];
+	if (typeof level['layout'][newPosY] != 'undefined' && typeof level['layout'][newPosY][newPosX] != 'undefined') {
+		var move = false;
+		if (level['layout'][newPosY][newPosX] == ' ') {
+			move = true;
+		} else if (level['layout'][newPosY][newPosX] == level['layout'][newPosY][newPosX].toUpperCase()) {
+			state['color'] = level['layout'][newPosY][newPosX].toLowerCase();
+			move = true;
+		}
 
-		update(stage, level);
+		if (move) {
+			state['position'][0] = newPosX;
+			state['position'][1] = newPosY;
+			level['layout'][newPosY][newPosX] = state['color'];
+
+			check(stage, level);
+			update(stage, level);
+		}
+	}
+}
+
+function check(stage) {
+	var level = state['level'];
+
+	level['goalsLeft'] = $.extend({}, levels[state['levelNumber']]['goalsLeft']);
+	for(var i = 0; i < level['layout'].length; i++) {
+		for(var j = 0; j < level['layout'][i].length; j++) {
+			var tile = level['layout'][i][j].toLowerCase();
+
+			if (tile != ' ') {
+				if (level['goalsLeft'][tile] > 0) {
+					level['goalsLeft'][tile]--;
+					level['goalsLeft']['total']--;
+				}
+			}
+		}
+	}
+
+	console.log(level['goalsLeft']['total']);
+
+	if (level['goalsLeft']['total'] == 0) {
+		nextLevel();
+	}
+}
+
+function nextLevel() {
+	var next = state['levelNumber'] + 1;
+
+	console.log(next);
+
+	if (next <= 3) {
+		state['level'] = $.extend({}, levels[next]);
+		state['levelNumber'] = next;
+		state['position'] = state['level']['start'];
+		state['color'] = state['level']['layout'][state['position'][0]][state['position'][1]];
 	}
 }
